ff18ff1cee403a18f5452e44a4d770c84b73c4e5
HanLong <404402223@qq.com>
2016-01-14 18:25:06 +0800

    bug

--- app/src/main/java/com/ithaha/systemwidget/scroll/DragView1Activity.java
+++ app/src/main/java/com/ithaha/systemwidget/scroll/DragView1Activity.java
@@ -1,15 +1,17 @@
 package com.ithaha.systemwidget.scroll;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.ithaha.systemwidget.R;
 
-public class DragView1Activity extends AppCompatActivity {
+public class DragView1Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_drag_view1);
     }
 }
